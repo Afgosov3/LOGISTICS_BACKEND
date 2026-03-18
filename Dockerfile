16d037d25449b312ba510c2e717f0d1a5e7fadb0
Afgosov3 <shamsiddin777zzz@gmail.com>
2026-03-18 12:00:07 +0500

    docker consturction for server

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ ENV PYTHONUNBUFFERED 1
 WORKDIR /app
 
 RUN pip install --upgrade pip
+
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
+echo "Applying migrations..."
 python manage.py migrate --noinput
-python manage.py create_default_dispatchers
-python manage.py runserver 0.0.0.0:8000
 
+echo "Creating default dispatchers..."
+python manage.py create_default_dispatchers || true
+
+echo "Starting Gunicorn..."
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 4
